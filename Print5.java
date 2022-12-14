23d42188d11811d2a761e7d12cadbe30fc69242f
Vicky5453 <113967961+Vicky5453@users.noreply.github.com>
2022-12-14 21:57:03 +0530

    Add files via upload

---
+++ Print5.java
@@ -0,0 +1,25 @@
+package whil.e;
+
+public class Print5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int no=1;
+           int i=1;
+           int j=2;
+           while (no<=10)
+           {
+        	   if(i<=9)
+        	   {
+        		   System.out.print(i+" ");
+            	   i=i+2;  
+        	   }
+        	   else if(j<=10)
+        	   {
+        		   System.out.print(j+" ");
+              	 j=j+2;
+        	   }
+           }
+	}
+}
+          //  1 3 5 7 9   2 4 6 8 10
